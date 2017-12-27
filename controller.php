f940768a4d8bf08f89f547ff3a569a11633171d5
a3020 <a3020@fastmail.com>
2017-12-27 15:12:02 +0100

    Update changelog / version bump

--- controller.php
+++ controller.php
@@ -14,7 +14,7 @@ final class Controller extends Package
 {
     protected $pkgHandle = 'centry';
     protected $appVersionRequired = '8.0';
-    protected $pkgVersion = '2.1.5';
+    protected $pkgVersion = '2.1.6';
     protected $pkgAutoloaderRegistries = [
         'src/Centry' => '\A3020\Centry',
     ];
