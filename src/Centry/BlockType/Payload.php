c9e38cb2960b91e80646b494731380530a510a3f
a3020 <a3020@fastmail.com>
2018-02-16 14:56:05 +0100

    Improve blocktype payload by retrieving handles from c5 directory

--- src/Centry/BlockType/Payload.php
+++ src/Centry/BlockType/Payload.php
@@ -46,50 +46,9 @@ final class Payload extends PayloadAbstract
 
     private function getDefaultBlockTypeHandles()
     {
-        return [
-            "core_area_layout",
-            "core_page_type_composer_control_output",
-            "core_scrapbook_display",
-            "core_stack_display",
-            "core_conversation",
-            "autonav",
-            "content",
-            "date_navigation",
-            "external_form",
-            "file",
-            "page_attribute_display",
-            "form",
-            "page_title",
-            "feature",
-            "topic_list",
-            "social_links",
-            "testimonial",
-            "share_this_page",
-            "google_map",
-            "html",
-            "horizontal_rule",
-            "image",
-            "faq",
-            "next_previous",
-            "page_list",
-            "rss_displayer",
-            "search",
-            "image_slider",
-            "survey",
-            "switch_language",
-            "tags",
-            "video",
-            "youtube",
-            "express_form",
-            "express_entry_list",
-            "express_entry_detail",
-            "desktop_site_activity",
-            "desktop_app_status",
-            "desktop_featured_theme",
-            "desktop_featured_addon",
-            "desktop_newsflow_latest",
-            "desktop_latest_form",
-            "desktop_waiting_for_me",
-        ];
+        // concrete/blocks
+        $directory = DIR_BASE_CORE . DIRECTORY_SEPARATOR . DIRNAME_BLOCKS;
+
+        return array_values(array_diff(scandir($directory), array('..', '.')));
     }
 }
